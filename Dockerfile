96330d4807dd2783d586e681c91c54b95723fc95
leftsky <left.sky@vip.qq.com>
2021-03-01 13:47:19 +0800

    更新

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM leftsky/php
+LABEL maintainer="leftsky <leftsky@vip.qq.com>" 
+
+COPY get_ssl.sh /home/
+RUN chmod a+x /home/get_ssl.sh
+RUN apk add certbot tcl tk expect
 
 EXPOSE 433 80
 WORKDIR /var/www
-
-
-RUN []
---
+++ get_ssl.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/expect
+
+# 拉取域名参数
+set domains [lindex $argv 0]
+# 拉取邮箱参数
+set email [lindex $argv 1]
+# 设置超时
+set timeout 30
+# 开始申请证书
+spawn certbot --server https://acme-v02.api.letsencrypt.org/directory -d "$domains" --manual --preferred-challenges http-01 certonly
+# 输入邮箱，同意获得IP
+expect "Enter email address" { send "$email\r"; exp_continue; } \
+"agree in order to register with the ACME server" { send "A\r"; exp_continue; } \
+"share your email" { send "N\r"; exp_continue; } \
+"Are you OK with your IP being logged" { send "Y\r"; }
+# 获得验证文件内容
+expect -re "(\\S{70,90})"
+set file_content $expect_out(0,string)
+# expect -re "(http\\S{40,90})"
+# set file_url $expect_out(0,string)
+expect -re "http\\S+/(\\S{10,90})"
+# send_user $expect_out(0,string)
+# send_user $expect_out(1,string)
+set file_name $expect_out(1,string)
+send_user "\r\n校验文件内容：$file_content"
+# send_user "\r\n校验文件路径：$file_url"
+send_user "\r\n校验文件名：$file_name"
+exec echo "$file_content" > $file_name
+send "\r"
+interact
+
